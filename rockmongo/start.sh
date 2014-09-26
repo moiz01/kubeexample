afe9417e25b7c338c0d90b28aba4cbb9dcea052d
Scott Collier <emailscottcollier@gmail.com>
2014-09-26 11:14:22 -0400

    removing start.sh, sending to localhost is working

--- rockmongo/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Tell the rockmongo service how to get to the MongoDB container
-# the SERVICE_HOST var is provided by kubernetes
-sed -i "s/mongo_host\"] = \"127.0.0.1\"/mongo_host\"] = \"0.0.0.0\"/" /var/www/html/config.php
-
-# Start the Apache server
-/usr/sbin/httpd -D FOREGROUND
