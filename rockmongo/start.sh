9273ddb32fbd590500f76b2f70c601c0adea1874
Scott Collier <emailscottcollier@gmail.com>
2014-09-26 10:41:00 -0400

    removing host ports from mongodb container definition

--- rockmongo/start.sh
+++ rockmongo/start.sh
@@ -2,7 +2,7 @@
 
 # Tell the rockmongo service how to get to the MongoDB container
 # the SERVICE_HOST var is provided by kubernetes
-sed -i "s/mongo_host\"] = \"127.0.0.1\"/mongo_host\"] = \"$SERVICE_HOST\"/" /var/www/html/config.php
+sed -i "s/mongo_host\"] = \"127.0.0.1\"/mongo_host\"] = \"0.0.0.0\"/" /var/www/html/config.php
 
 # Start the Apache server
 /usr/sbin/httpd -D FOREGROUND
